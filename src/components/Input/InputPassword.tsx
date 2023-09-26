ca717483a61cda61ba41439e75343bec57e75887
oviniciusrosa <bakavini99@gmail.com>
2023-09-26 19:27:48 -0300

    feat: create InputPassword component

---
+++ src/components/Input/InputPassword.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import * as Icon from "react-native-feather";
+import { Input } from "./Input";
+import { InputProps } from "./@types";
+import theme from "~/theme";
+
+import * as S from "./styles";
+
+const { colors } = theme;
+
+export function InputPassword(props: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  function changeVisibility() {
+    setShowPassword(prev => !prev);
+  }
+
+  return (
+    <S.PasswordWrapper>
+      <Input
+        style={{ paddingRight: 40 }}
+        secureTextEntry={!showPassword}
+        {...props}
+      />
+
+      <S.TouchableSuffix activeOpacity={0.7} onPress={changeVisibility}>
+        {showPassword ? (
+          <Icon.EyeOff color={colors.grey_80} width={20} />
+        ) : (
+          <Icon.Eye color={colors.grey_80} width={20} />
+        )}
+      </S.TouchableSuffix>
+    </S.PasswordWrapper>
+  );
+}
