863c63df72ee0523c89a57f570ee2846f9bf2226
oviniciusrosa <bakavini99@gmail.com>
2023-09-28 11:51:36 -0300

    style: added suitable color to screen background

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,23 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { LoginScreen } from "../screens/LoginScreen";
 import { HomeTabs } from "./tabs/HomeTabs";
+import theme from "~/theme";
 
 const RootStack = createStackNavigator();
 
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: theme.colors.white,
+  },
+};
+
 export function Routes() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <RootStack.Navigator>
         <RootStack.Screen
           name="Login"
