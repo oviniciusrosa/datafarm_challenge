1a03b7855340a60243a6cce4fa01efbff631181a
oviniciusrosa <bakavini99@gmail.com>
2023-09-28 19:41:29 -0300

    style: adjust bottom tab

--- src/routes/tabs/HomeTabs.tsx
+++ src/routes/tabs/HomeTabs.tsx
@@ -2,20 +2,37 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StopRegisterListScreen } from "../../screens/StopRegisterListScreen";
 import { StopRegistrationScreen } from "../../screens/StopRegistrationScreen";
 
+import * as Icons from "react-native-feather";
+import theme from "~/theme";
+
 const Tab = createBottomTabNavigator();
 
 export function HomeTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.primary_500,
+        headerShown: false,
+        tabBarShowLabel: false,
+
+        tabBarStyle: {
+          backgroundColor: theme.colors.white,
+          elevation: 0,
+        },
+      }}>
       <Tab.Screen
         name="StopRegisterList"
         component={StopRegisterListScreen}
-        options={{ headerShown: false }}
+        options={{
+          tabBarIcon: ({ color }) => <Icons.Clock color={color} />,
+        }}
       />
       <Tab.Screen
         name="StopRegistration"
         component={StopRegistrationScreen}
-        options={{ headerShown: false }}
+        options={{
+          tabBarIcon: ({ color }) => <Icons.List color={color} />,
+        }}
       />
     </Tab.Navigator>
   );
