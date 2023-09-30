2bbe2f15097788cf196ced0f0bdd076add870c16
oviniciusrosa <bakavini99@gmail.com>
2023-09-30 18:02:18 -0300

    fix: add react-native-get-random-values to fix uuid generation

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ import { name as appName } from "./app.json";
 
 import "reflect-metadata";
 import "react-native-gesture-handler";
+import "react-native-get-random-values";
 
 AppRegistry.registerComponent(appName, () => App);
