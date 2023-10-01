40bc8384ce57d1bc3846c29546050699aae6ab33
oviniciusrosa <bakavini99@gmail.com>
2023-10-01 06:15:51 -0300

    feat: add net info hook

--- App.tsx
+++ App.tsx
@@ -6,20 +6,23 @@ import AuthProvider from "~/contexts/authentication";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StopRunning } from "~/components";
+import ExecQueueProvider from "~/contexts/execution_queue";
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <ResourcesProvider>
-        <AuthProvider>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <BottomSheetModalProvider>
-              <StopRunning />
-              <Routes />
-            </BottomSheetModalProvider>
-          </GestureHandlerRootView>
-        </AuthProvider>
-      </ResourcesProvider>
+      <ExecQueueProvider>
+        <ResourcesProvider>
+          <AuthProvider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              <BottomSheetModalProvider>
+                <StopRunning />
+                <Routes />
+              </BottomSheetModalProvider>
+            </GestureHandlerRootView>
+          </AuthProvider>
+        </ResourcesProvider>
+      </ExecQueueProvider>
     </ThemeProvider>
   );
 }
